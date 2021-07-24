c1e6af0a754eb82a5b83a26f7d081c2839a563a6
Poojan <pra.poojan2000@gmail.com>
2021-07-24 11:13:44 +0530

    UI change

--- js/index.js
+++ js/index.js
@@ -1,15 +1,6 @@
-var header = document.getElementsByClassName("sidenav");
-var listitem = header.getElementsByClassName("nav-list");
-        var btns = header.getElementsByClassName("nav-name");
-        for (var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener("click", function () {
-                var current = document.getElementsByClassName("active");
-                if (current.length > 0) {
-                    current[0].className = current[0].className.replace(" active", "");
-                }
-                this.className += " active";
-            });
-        }
+
+
+
 
 function submit(){
     alert("This feature is currently unavailable.");
--- php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$databaseHost = "localhost";
-$databaseName = "myweb";
-$databaseUserName = "root";
-$databasePassword = "";
-
-$mysqli = mysqli_connect($databaseHost, $databaseUserName, $databasePassword, $databaseName);
-?>
